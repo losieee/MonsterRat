e0796c4cdb881083fd6606252ff4450add8859f0
shingumingi <pettid0314@naver.com>
2026-03-11 22:17:34 +0900

    [Fix] Client 중복 로그인 시 서버 접속 요청 중복 방지

--- MonsterRat_Unity/My project/Assets/Scripts/PhotonScript/LobbyManager.cs
+++ MonsterRat_Unity/My project/Assets/Scripts/PhotonScript/LobbyManager.cs
@@ -24,6 +24,8 @@ public class LobbyManager : MonoBehaviour, INetworkRunnerCallbacks
     public GameObject roomItemPrefab;
 
     private NetworkRunner _runner;
+    private bool hasJoinedLobby = false;
+    private bool isSubmittingNickname = false;
 
     void Start()
     {
@@ -54,28 +56,37 @@ public class LobbyManager : MonoBehaviour, INetworkRunnerCallbacks
 
     public async void OnClick_SubmitNickname()
     {
+        if (isSubmittingNickname) return;
         if (string.IsNullOrEmpty(nicknameInput.text)) return;
+
+        isSubmittingNickname = true;
 
         string name = nicknameInput.text;
         PlayerPrefs.SetString("PlayerName", name);
 
-
         InitializeRunner();
 
-        var result = await _runner.JoinSessionLobby(SessionLobby.ClientServer);
+        if (!hasJoinedLobby)    // 로비에 들어간 적 없을때만 실행 (접속 요청 중복 방지)
+        {
+            var result = await _runner.JoinSessionLobby(SessionLobby.ClientServer);
 
-        if (result.Ok)
-        {
-            Debug.Log("버튼 입력 완료 마스터 서버(로비) 입장 성공!");
-            if (goToRoomListButton != null) goToRoomListButton.interactable = true;
-        }
-        else
-        {
-            Debug.LogError($"로비 접속 실패: {result.ShutdownReason}");
+            if (result.Ok)
+            {
+                hasJoinedLobby = true;
+                Debug.Log("버튼 입력 완료 마스터 서버(로비) 입장 성공!");
+                if (goToRoomListButton != null) goToRoomListButton.interactable = true;
+            }
+            else
+            {
+                Debug.LogError($"로비 접속 실패: {result.ShutdownReason}");
+                isSubmittingNickname = false;
+                return;
+            }
         }
 
         LoginPanel.SetActive(false);
         MainPanel.SetActive(true);
+        isSubmittingNickname = false;
     }
 
     public void OnClick_GoToRoomList()
